cac38d7771df6c5995c5aa605d9b1efa96b3f902
yahelm165 <57379213+yahelm165@users.noreply.github.com>
2025-02-04 09:48:15 -0700

    Add files via upload

---
+++ Tree.py
@@ -0,0 +1,49 @@
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, node, key):
+        if key < node.key:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert_recursive(node.left, key)
+        else:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert_recursive(node.right, key)
+
+    def print_tree(self, node=None, level=0, prefix="Root: "):
+        if node is None:
+            node = self.root
+        if node is not None:
+            print(" " * (level * 4) + prefix + str(node.key))
+            if node.left or node.right:
+                if node.left:
+                    self.print_tree(node.left, level + 1, "L--- ")
+                else:
+                    print(" " * ((level + 1) * 4) + "L--- None")
+                if node.right:
+                    self.print_tree(node.right, level + 1, "R--- ")
+                else:
+                    print(" " * ((level + 1) * 4) + "R--- None")
+
+# Ejemplo de uso
+my_tree = Tree()
+my_tree.insert(50)
+my_tree.insert(30)
+my_tree.insert(70)
+my_tree.insert(20)
+my_tree.insert(40)
+my_tree.insert(60)
+my_tree.insert(80)
+
+print("Árbol de búsqueda binario: \n")
+my_tree.print_tree()
